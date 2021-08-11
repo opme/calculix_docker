5bbc03c9cb7a85c58a9008c6796ae03a59b06769
Michael Shamberger <ext-michael.shamberger@finago.com>
2021-08-11 11:26:59 +0300

    parsec working now

--- docker/make_scotch.sh
+++ docker/make_scotch.sh
@@ -2,7 +2,7 @@
 
 umask 022
 cd src
-ln -sf Make.inc/Makefile.inc.x86-64_pc_linux2 Makefile.inc
+cp Make.inc/Makefile.inc.x86-64_pc_linux2 Makefile.inc
 sed -i '/CFLAGS/ s/$/ -DINTSIZE64/' Makefile.inc
 
 make -j8 scotch
